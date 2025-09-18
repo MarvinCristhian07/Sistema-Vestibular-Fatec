9bd59f752aad98395db04d13f94d9758a463dce3
Marvin Cristhian <marvincristhian07.contato@gmail.com>
2025-09-18 17:34:56 -0300

    Update main.py

--- Prova_Orlando_2semestre/main.py
+++ Prova_Orlando_2semestre/main.py
@@ -3,6 +3,7 @@ from Estrutura_de_dados.aplicadores import *
 
 lista_candidatos = ListaCandidatos()
 lista_aplicadores = ListaAplicadores()
+lista_aprovados = ListaAprovados()
 
 print("")
 print("  ███████            ███████      █      ███████ ███████ ███████ ███████ ███████      █      ███████ ███████")
@@ -19,11 +20,12 @@ while True:
     print("\nBem vindo(a) ao Sistema Gerenciador de Vestibular FATEC 2026 1° Semestre!              ")
     print("Selecione a opção que deseja realizar:                                                   ")
     print("                                                                                         ")
-    print("1 - Inscrever candidato(a)                   |   5 - Cadastrar funcionário(a)            ")
-    print("2 - Editar informações do(a) candidato(a)    |   6 - Visualizar lista de funcionários(as)")
-    print("3 - Confirmar pagamento do(a) candidato(a)   |   7 - .                                   ")
-    print("4 - Visualizar lista de candidatos           |   8 - .                                   ")
-    print("0 - Sair                                     |                                           ")
+    print("1 - Inscrever candidato(a)                   |   7 - Visualizar salas necessárias        ")
+    print("2 - Editar informações do(a) candidato(a)    |   8 - Listar candidatos por sala          ")
+    print("3 - Confirmar pagamento do(a) candidato(a)   |   9 - Relação candidatos/vaga (efetivados)")
+    print("4 - Visualizar lista de candidatos           |   10 - Gerenciar aprovações               ")
+    print("5 - Cadastrar funcionário(a)                 |   11 - Listar aprovados                   ")
+    print("6 - Visualizar lista de funcionários(as)     |   0 - Sair do sistema                     ")
     print("                                                                                         ")
 
     escolha = input("Escolha: ")
@@ -62,6 +64,16 @@ while True:
         lista_aplicadores.adicionar_aplicador(nome_aplicador, cargo_aplicador)
     elif escolha == "6":
         lista_aplicadores.listar_aplicadores()
+    elif escolha == "7":
+        lista_candidatos.calcular_salas_necessarias()
+    elif escolha == "8":
+        lista_candidatos.gerar_lista_efetivados().listar_por_sala()
+    elif escolha == "9":
+        lista_candidatos.relacao_candidatos_efetivados()
+    elif escolha == "10":
+        lista_candidatos.gerenciar_aprovacao(lista_aprovados)
+    elif escolha == "11":
+        lista_aprovados.listar_aprovados()
     elif escolha == "0":
         print("\nSaindo do sistema...")
         break
